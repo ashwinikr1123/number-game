2f3d9023425b3ae9e322749be24b04443a9f9ad1
unknown <kr.ashwini1123@gmail.com>
2024-01-21 13:32:29 +0530

    improvised-writing-of-the-logic

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 'use  strict';
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
-let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let initialScore = 20;
+
+const number = document.querySelector('.number');
+
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+const highscore = document.querySelector('.highscore');
 
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number!';
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high!';
-    score--;
-    document.querySelector('.score').textContent = score;
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too low!';
-    score--;
-    document.querySelector('.score').textContent = score;
-  } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '✔ Correct Number';
-    document.querySelector('.highscore').textContent = score;
+    message.textContent = '⛔ No Number!';
+  }
+  //    else if (guess > secretNumber) {
+  //     message.textContent.textContent = '📈 Too high!';
+  //     initialScore--;
+  //     score.textContent = initialScore;
+  //   } else if (guess < secretNumber) {
+  //     message.textContent = '📉 Too low!';
+  //     initialScore--;
+  //     score.textContent = initialScore;
+  //   } else if (guess === secretNumber) {
+  //     message.textContent = '✔ Correct Number';
+  //     document.querySelector('.highscore').textContent = initialScore;
+  //   }
+
+  // DRY way
+  if (guess === secretNumber) {
+    number.textContent = secretNumber;
+    message.textContent = '✔ Correct Number';
+    highscore.textContent = initialScore;
+  } else {
+    // guess > secretNumber
+    //   ? (message.textContent = '📈 Too high!')
+    //   : (message.textContent = '📉 Too low!');
+    message.textContent = guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+    initialScore--;
+    score.textContent = initialScore;
   }
 });
